6092d4bd145646373b0b67a4a57c3439312f9616
rwoudstra <robinwoudstra@gmail.com>
2016-12-12 13:11:33 +0100

    Update server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(express.static(path.join(__dirname,'public')));
 app.use(express.static(path.join(__dirname,'views')));
 
 // Create a Node.js based http server on port 80 (or 3000 for local testing)
-var server = require('http').createServer(app).listen(3000);
+var server = require('http').createServer(app).listen(80);
 
 // Create a Socket.IO server and attach it to the http server
 var io = require('socket.io').listen(server);
